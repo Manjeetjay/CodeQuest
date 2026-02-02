4bab2eb6d0641e2d0e0b9d6758b9f67136beda44
Manjeetjay <manjeetjay00@gmail.com>
2026-02-03 04:46:10 +0530

    changed the baseurl of backend from hardcoded to env variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ import { logger } from "../utils/logger";
 
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
